8568bd12e9735ed7f07f24d229100614884cb6a7
xNikhil001 <naikn8113@gmail.com>
2023-02-27 23:43:20 +0530

    initial push

---
+++ hangman/lib/hangman.ex
@@ -0,0 +1,13 @@
+defmodule Hangman do
+
+  alias Hangman.Type
+  alias Hangman.Impl.Game
+
+  @opaque game :: Game.t
+
+  @spec new_game() :: game
+  defdelegate new_game, to: Game
+
+  @spec make_move(game, String.t()) :: {game, Type.tally}
+  defdelegate make_move(game,guess), to: Game
+end
